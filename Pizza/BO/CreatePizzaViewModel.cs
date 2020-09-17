c08351833715f0631ee72063b7652dde5017f50f
Lucas <brientlucas56@gmail.com>
2020-09-17 12:34:40 +0200

    Suite pizza

---
+++ Pizza/BO/CreatePizzaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class CreatePizzaViewModel
+    {
+        public Pizza Pizza { get; set; }
+        public List<Pate> Pates { get; set; }
+        public int IdPate { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public int IdIngredient { get; set; }
+
+    }
+}
---
+++ Pizza/BO/EditPizzaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class EditPizzaViewModel
+    {
+        public Pizza Pizza { get; set; }
+        public Pate Pate { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+
+    }
+}
